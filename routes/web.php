f7594a3a0142121613c051920147f36b3e1a8661
Wendel <w-endel@hotmail.com>
2020-07-18 22:37:13 -0300

    Ajustando Rotas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'teste'], function () {
+    Route::get('/', function () {
+        return view('teste');
+    });
+});
+
+
+
+
+Route::get('/teste', function () {
+    return view('teste');
+});
